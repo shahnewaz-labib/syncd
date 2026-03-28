1a1c5bf32a7ac12460db7fd02fe86c886c413fa3
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2026-03-28 17:47:41 +0600

    add transfer config constants
    transfer port, chunk size, and timeout settings.

--- config/config.go
+++ config/config.go
@@ -6,9 +6,14 @@ const (
 	// Network configuration
 	DISCOVERY_PORT = 9999
 	API_PORT       = "10000"
+	TRANSFER_PORT  = 10001
 
 	// Announcement settings
 	BROADCAST_INTERVAL = 5 * time.Second
 	DEVICE_TIMEOUT     = 15 * time.Second
 	SYNCD_DISCOVER     = "SYNCD_DISCOVER"
+
+	// Transfer settings
+	TRANSFER_CHUNK_SIZE = 64 * 1024 // 64KB
+	TRANSFER_TIMEOUT    = 30 * time.Second
 )
